e45614b7364cc8af60b2607274193bd27a9ffd26
rubenhoba28 <rubenhoba07@gmail.com>
2024-03-12 17:18:40 +0200

    last update

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 
 def get_pdf_text(pdf_docs):
-    text = ""
+    texts = ""
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
+            texts += page.extract_text()
+    return texts
 
 
 def get_text_chunks(text):
